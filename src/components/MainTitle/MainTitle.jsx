3bb9ba15894bfd818cfddc92e2dfb0c6d504277f
박요진 <syon013@naver.com>
2024-02-06 19:41:49 +0900

    🚑️ main-title Component 변경
    1. png -> svg 파일로 변경

--- src/components/MainTitle/MainTitle.jsx
+++ src/components/MainTitle/MainTitle.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Arrow } from '@svg/right_arrow.svg';
 import './MainTitle.scss';
 
 const MainTitle = ({ link, title }) => {
   return (
     <h2 className="main-title">
-      <Link to={link}>{title}</Link>
+      <Link to={link}>
+        {title}
+        <span className="main-title-svg">
+          <Arrow />
+        </span>
+      </Link>
     </h2>
   );
 };
