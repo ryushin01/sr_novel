9601e09a648d0bc7fba00f07f1486f80896ff6eb
박요진 <syon013@naver.com>
2024-02-06 17:21:42 +0900

    🚧 작업 진행 중 임시 커밋
    1. API_URL 추가
    
    2. Main UI 변경 중
    
    3. Best및 New 관련 scss Typography 및 테마 관련 수정

--- src/assets/data/ApiUrl.jsx
+++ src/assets/data/ApiUrl.jsx
@@ -3,5 +3,7 @@ const API_KEY = import.meta.env.VITE_ALADDIN_API_KEY;
 export const API_URL = {
   BEST_SELLER: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=Bestseller&MaxResults=10&start=1&SearchTarget=Book&output=js&Cover=Big&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
   NEW_BOOK: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=itemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Cover=Big&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
-  SPECIAL: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=ItemNewSpecial&MaxResults=10&start=1&SearchTarget=Book&output=js&Cover=Big&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
+  SPECIAL: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=ItemNewSpecial&MaxResults=5&start=1&SearchTarget=Book&output=js&Cover=MidBig&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
+  EDITOR: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=ItemEditorChoice&MaxResults=5&start=1&SearchTarget=Book&output=js&Cover=MidBig&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
+  BLOG: `ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=BlogBest&MaxResults=5&start=1&SearchTarget=Book&output=js&Cover=MidBig&CategoryId=51122&OptResult=ebookList,usedList,fileFormatList&Version=20131101`,
 };
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
-import { API_URL } from '@data/ApiUrl';
+import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 import { customAxios } from '../../config';
+import { API_URL } from '@data/ApiUrl';
 
 import BestBand from './Components/BestBand/BestBand';
 import BigBanner from './Components/BigBanner/BigBanner';
@@ -16,6 +17,8 @@ const Main = () => {
   const [bsBooksData, setBsBooksData] = useState([]);
   const [newBooksData, setNewBooksData] = useState([]);
   const [specialBooksData, setSpecialBooksData] = useState([]);
+  const [editorBooksData, setEditorBooksData] = useState([]);
+  const [blogBooksData, setBlogBooksData] = useState([]);
   const [loading, setLoading] = useState(true);
   // VITE에서 환경변수를 사용하기 위해서는 import.meta.env를 사용해야 한다.
 
@@ -23,6 +26,8 @@ const Main = () => {
     API_URL.BEST_SELLER,
     API_URL.NEW_BOOK,
     API_URL.SPECIAL,
+    API_URL.EDITOR,
+    API_URL.BLOG,
   ];
 
   useEffect(() => {
@@ -38,6 +43,8 @@ const Main = () => {
       setBsBooksData(response[0]?.data?.item);
       setNewBooksData(response[1]?.data?.item);
       setSpecialBooksData(response[2]?.data?.item);
+      setEditorBooksData(response[3]?.data?.item);
+      setBlogBooksData(response[4]?.data?.item);
 
       setLoading(false);
     } catch (error) {
@@ -45,12 +52,94 @@ const Main = () => {
     }
   };
 
+  console.log(blogBooksData);
+
   return (
     <>
       {loading && <Loading />}
       <main className="main-container">
         <section className="main-body-wrap">
           <BigBanner />
+          <div className="book-list-wrap">
+            <div className="new-special-list">
+              <div className="new-special-main-title">
+                <Link to="#">
+                  <h2>[NEW] 주목할만한 신간 리스트</h2>
+                </Link>
+              </div>
+              <ul>
+                {specialBooksData.map(
+                  ({ author, itemId, cover, title, pubDate }) => {
+                    const authorSplit = author.split('(지은이)');
+                    return (
+                      <li key={itemId}>
+                        <div className="new-special-img">
+                          <img src={cover} alt={title} />
+                        </div>
+                        <div className="new-special-title">
+                          <h3>{title}</h3>
+                          <h4>{authorSplit}</h4>
+                          <span>출판일 : {pubDate}</span>
+                        </div>
+                      </li>
+                    );
+                  },
+                )}
+              </ul>
+            </div>
+            <div className="new-special-list">
+              <div className="new-special-main-title">
+                <Link to="#">
+                  <h2>[NEW] 주목할만한 신간 리스트</h2>
+                </Link>
+              </div>
+              <ul>
+                {editorBooksData.map(
+                  ({ author, itemId, cover, title, pubDate }) => {
+                    const authorSplit = author.split('(지은이)');
+                    return (
+                      <li key={itemId}>
+                        <div className="new-special-img">
+                          <img src={cover} alt={title} />
+                        </div>
+                        <div className="new-special-title">
+                          <h3>{title}</h3>
+                          <h4>{authorSplit}</h4>
+                          <span>출판일 : {pubDate}</span>
+                        </div>
+                      </li>
+                    );
+                  },
+                )}
+              </ul>
+            </div>
+            <div className="new-special-list">
+              <div className="new-special-main-title">
+                <Link to="#">
+                  <h2>[NEW] 주목할만한 신간 리스트</h2>
+                </Link>
+              </div>
+              <ul>
+                {blogBooksData.map(
+                  ({ author, itemId, cover, title, pubDate }) => {
+                    const authorSplit = author.split('(지은이)');
+                    return (
+                      <li key={itemId}>
+                        <div className="new-special-img">
+                          <img src={cover} alt={title} />
+                        </div>
+                        <div className="new-special-title">
+                          <h3>{title}</h3>
+                          <h4>{authorSplit}</h4>
+                          <span>출판일 : {pubDate}</span>
+                        </div>
+                      </li>
+                    );
+                  },
+                )}
+              </ul>
+            </div>
+          </div>
           <div className="content-wrap">
             <NewBook newBooksData={newBooksData} />
             <BestBand booksData={bsBooksData} />
