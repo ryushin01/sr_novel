a6eb151fae28f481942606380c4c9d5475bd91fe
박요진 <syon013@naver.com>
2024-02-07 23:28:40 +0900

    🎨 명칭 변경 및 className 변경

---
+++ src/pages/Main/Components/BookListItems/components/ListContentsImg.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const ListContentsImg = ({ bookData }) => {
+  const { isbn13, title, cover } = bookData;
+
+  return (
+    <div className="book-list-contents-img">
+      <Link to={`/detail:${isbn13}`}>
+        <img src={cover} alt={title} />
+      </Link>
+    </div>
+  );
+};
+
+export default ListContentsImg;
