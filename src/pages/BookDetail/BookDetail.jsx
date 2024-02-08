672cdb074778cdbda50ae4be59b34369bc36ee3b
박요진 <syon013@naver.com>
2024-02-08 14:48:37 +0900

    🚧 detail Page 작업 중

--- src/pages/BookDetail/BookDetail.jsx
+++ src/pages/BookDetail/BookDetail.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const BookDetail = () => {
-  const [bookIsbn, setBookIsbn] = useState('');
+import { customAxios } from '../../config';
 
-  const { id } = useParams();
-  console.log(id);
+import './BookDetail.scss';
+
+const BookDetail = () => {
+  const [bookItemInfo, setBookItemInfo] = useState([]);
+
+  const params = useParams();
+  const API_KEY = import.meta.env.VITE_ALADDIN_API_KEY;
+  const API_URL = `ttb/api/ItemLookUp.aspx?ttbkey=${API_KEY}&itemIdType=ISBN&ItemId=${params.isbn13}&output=js&Version=20131101&
+  OptResult=ebookList,usedList,reviewList`;
+
+  useEffect(() => {
+    getBookDetailInfo();
+  }, []);
+
+  const getBookDetailInfo = async () => {
+    try {
+      const response = await customAxios.get(API_URL);
+      setBookItemInfo(response?.data?.item);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div>
-      <h1>Book Detail</h1>
-    </div>
+    <main className="book-detail-container">
+      <section className="book-detail-info-wrap"></section>
+      <section className="book-detail-episode-list-wrap"></section>
+    </main>
   );
 };
 
--- src/pages/Main/Components/BookListItems/components/ListContentsImg.jsx
+++ src/pages/Main/Components/BookListItems/components/ListContentsImg.jsx
@@ -5,7 +5,7 @@ const ListContentsImg = ({ bookData }) => {
 
   return (
     <div className="book-list-contents-img">
-      <Link to={`/detail:${isbn13}`}>
+      <Link to={`/detail/${isbn13}`}>
         <img src={cover} alt={title} />
       </Link>
     </div>
