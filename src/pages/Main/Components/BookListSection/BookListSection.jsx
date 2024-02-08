2fce33ef3f013e4f44993d7d247a5dbd181a3958
박요진 <syon013@naver.com>
2024-02-08 11:57:00 +0900

    🎨 북 랭킹 관련 컴포넌트 코드 구조 변경

--- src/pages/Main/Components/BookListSection/BookListSection.jsx
+++ src/pages/Main/Components/BookListSection/BookListSection.jsx
@@ -7,12 +7,19 @@ import MainTitle from '../MainTitle/MainTitle';
  * @property {string} title - Main-Title에 표시할 텍스트를 입력합니다.
  * @property {array} bookData - 화면에 표시할 책 데이터에 대한 배열을 입력합니다.
  * @property {string} direction - List의 방향을 입력합니다. (가로: true, 세로: false) 디폴트 값은 false 입니다.
+ * @property {string} rank - List의 순위를 입력합니다. (순위: true, 순위 없음: false) 디폴트 값은 false 입니다.
  */
-const BookListSection = ({ link, title, bookData, direction }) => {
+const BookListSection = ({
+  link,
+  title,
+  bookData,
+  direction = false,
+  rank = false,
+}) => {
   return (
     <div className="book-list-wrap">
       <MainTitle to={link} title={title} />
-      <BookList bookData={bookData} direction={direction} />
+      <BookList bookData={bookData} direction={direction} rank={rank} />
     </div>
   );
 };
