d0317ba8fc94f58f0d3d249f63b1d6fbf2e7b80c
박요진 <syon013@naver.com>
2024-02-09 14:46:30 +0900

    🚧 Detail 페이지 작업 진행 중

--- src/pages/BookDetail/BookDetail.jsx
+++ src/pages/BookDetail/BookDetail.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { customAxios } from '../../config';
+import Button from '@components/Button/Button';
 
 import './BookDetail.scss';
+import ToggleButton from './ToggleButton/ToggleButton';
 
 const BookDetail = () => {
   const [bookItemInfo, setBookItemInfo] = useState([]);
+  const [descriptionToggle, setDescriptionToggle] = useState(true);
 
   const params = useParams();
+  const navigate = useNavigate();
   const API_KEY = import.meta.env.VITE_ALADDIN_API_KEY;
-  const API_URL = `ttb/api/ItemLookUp.aspx?ttbkey=${API_KEY}&itemIdType=ISBN&ItemId=${params.isbn13}&output=js&Version=20131101&
-  OptResult=ebookList,usedList,reviewList`;
+  const API_URL = `ttb/api/ItemLookUp.aspx?ttbkey=${API_KEY}&itemIdType=ISBN&ItemId=${params.isbn13}&Cover=Big&output=js&Version=20131101&
+  OptResult=ebookList,usedList,fileFormatList,previewImgList`;
 
   useEffect(() => {
     getBookDetailInfo();
@@ -26,10 +30,69 @@ const BookDetail = () => {
     }
   };
 
+  const handleDescriptionToggle = () => {
+    setDescriptionToggle(!descriptionToggle);
+  };
+
+  console.log(bookItemInfo);
+
   return (
     <main className="book-detail-container">
-      <section className="book-detail-info-wrap"></section>
-      <section className="book-detail-episode-list-wrap"></section>
+      <section className="book-detail-info-wrap">
+        {bookItemInfo.map(info => {
+          return (
+            <div className="book-detail-info" key={info.isbn13}>
+              <div className="book-detail-info-img">
+                <img src={info.cover} alt={info.title} />
+              </div>
+              <div className="book-detail-info-text">
+                <span className="book-detail-info-category">
+                  {info.categoryName}
+                </span>
+                <h2 className="book-detail-info-title">{info.title}</h2>
+                <span className="book-detail-info-author">
+                  {info.author} / {info.publisher}
+                </span>
+                <span className="book-detail-info-standard-price">
+                  정가 : {info.priceStandard.toLocaleString()}원
+                </span>
+                <span className="book-detail-info-sales-price">
+                  판매가 : {info.priceSales.toLocaleString()}원
+                </span>
+                <div className="book-detail-info-description">
+                  <p className={`${descriptionToggle ? 'active' : ''}`}>
+                    {info.description}
+                  </p>
+                  <ToggleButton
+                    handleDescriptionToggle={handleDescriptionToggle}
+                    descriptionToggle={descriptionToggle}
+                  />
+                </div>
+                <div className="book-detail-info-hashtag">
+                  <Button size="small" content="#판타지" color="grayscale" />
+                  <Button size="small" content="#환상소설" color="grayscale" />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+      <section className="book-detail-content-buy-wrap">
+        <h3 className="book-detail-content-buy">보유 이용권 : 0 개</h3>
+        {bookItemInfo.map(({ link }) => {
+          return (
+            <Button
+              key={link}
+              size="small"
+              content="구매하기"
+              color="primary"
+              onClick={() => {
+                window.location.href = link; // onClick 시 외부링크로 이동하기 위한 방법
+              }}
+            />
+          );
+        })}
+      </section>
     </main>
   );
 };
