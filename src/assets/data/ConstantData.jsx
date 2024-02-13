dbcd07b4367eba1cc22f067e781538b4ce4220e3
박요진 <syon013@naver.com>
2024-02-13 22:48:39 +0900

    📝 Select Box용 상수데이터 수정

--- src/assets/data/ConstantData.jsx
+++ src/assets/data/ConstantData.jsx
@@ -1,17 +1,17 @@
-export const BEST_SELLER_DATA = [
+export const LIST_SELECT_DATA = [
   {
     id: 1,
-    name: '최신순',
-    value: '최신순',
+    name: '판타지',
+    value: '51122',
   },
   {
     id: 2,
-    name: '조회순',
-    value: '조회순',
+    name: '로맨스',
+    value: '51125',
   },
   {
     id: 3,
-    name: '리뷰순',
-    value: '리뷰순',
+    name: '무협',
+    value: '50932',
   },
 ];
