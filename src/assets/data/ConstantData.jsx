7aab6acf4dd0a1656ee786da97efcbf0cc7624e6
박요진 <syon013@naver.com>
2024-02-19 23:02:40 +0900

    📝 상수 데이터 추가

--- src/assets/data/ConstantData.jsx
+++ src/assets/data/ConstantData.jsx
@@ -15,3 +15,32 @@ export const LIST_SELECT_DATA = [
     value: '50932',
   },
 ];
+
+
+export const EMAIL_SELECT_DATA = [
+  {
+    id: 1,
+    name: 'naver.com',
+    value: 'naver.com',
+  },
+  {
+    id: 2,
+    name: 'gmail.com',
+    value: 'gmail.com',
+  },
+  {
+    id: 3,
+    name: 'hanmail.net',
+    value: 'hanmail.net',
+  },
+  {
+    id: 4,
+    name: 'nate.com',
+    value: 'nate.com',
+  },
+  {
+    id: 5,
+    name: 'daum.net',
+    value: 'daum.net',
+  },
+];
