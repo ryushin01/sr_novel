37e76f76114aefc8f8868d4e629805c07afb6787
박요진 <syon013@naver.com>
2024-02-20 15:53:00 +0900

    🚧 회원가입 페이지 작업 진행 중

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,64 +1,131 @@
+import { EMAIL_SELECT_DATA } from '@data/ConstantData';
+import { RADIO_DATA } from '@data/ConstantData';
 import Input from '@components/Input/Input';
 import SelectBox from '@components/SelectBox/SelectBox';
 import Button from '@components/Button/Button';
-import {EMAIL_SELECT_DATA} from '@data/ConstantData';
+import RadioGroup from '@components/RadioGroup/RadioGroup';
 import './SignUp.scss';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [radioChecked, setRadioChecked] = useState('');
+
+  const handleRadioChange = e => {
+    if (e.target.checked) {
+      setRadioChecked(e.target.value);
+      return { ...RADIO_DATA, checked: true };
+    } else {
+      return { ...RADIO_DATA, checked: false };
+    }
+  };
+
   return (
-    <main className='sign-up-container'>
+    <main className="sign-up-container">
       <div className="sign-up-inner-wrap">
-        <section className='sign-up-form-section'>
-            <form>
-                <fieldset>
-                    <legend>회원가입</legend>
-                        <h2 className='sign-up-form-title'>회원정보</h2>
-                        <div className='sign-up-form-wrap'>
+        <section className="sign-up-form-section">
+          <form>
+            <fieldset>
+              <legend>회원가입</legend>
+              <h2 className="sign-up-form-title">회원정보</h2>
+              <div className="sign-up-form-wrap">
+                <div className="sign-up-id-wrap">
+                  <Input
+                    type="text"
+                    contents="아이디(이메일)"
+                    placeholder="아이디"
+                    purpose="sign-up"
+                    size="small"
+                    require
+                  />
 
-                         <div className="sign-up-id-wrap">
-                            <Input type='text' contents='아이디(이메일)' placeholder='아이디' purpose="sign-up" size="small" require/>
+                  <span> @ </span>
 
-                            <span> @ </span>
+                  <SelectBox data={EMAIL_SELECT_DATA} />
 
-                            <SelectBox data={EMAIL_SELECT_DATA}/>
-                        
-                            <Button type='button' content='중복확인' color="primary" size="small"/>
-                        </div>   
+                  <Button
+                    type="button"
+                    content="중복확인"
+                    color="primary"
+                    size="small"
+                  />
+                </div>
 
-                        <div className='sign-up-password-wrap'>
-                          <Input type='password' contents='비밀번호' placeholder='비밀번호' purpose="sign-up" size="small" require/>
-                          <span>영문,숫자,특수문자(@#$%^&*) 혼합하여 8자리 이상</span>
-                        </div>
+                <div className="sign-up-password-wrap">
+                  <Input
+                    type="password"
+                    contents="비밀번호"
+                    placeholder="비밀번호"
+                    purpose="sign-up"
+                    size="small"
+                    require
+                  />
+                  <span>영문,숫자,특수문자(@#$%^&*) 혼합하여 8자리 이상</span>
+                </div>
 
-                        <Input type='password' contents='비밀번호 확인' placeholder='비밀번호 확인' purpose="sign-up" size="small" require/>
+                <Input
+                  type="password"
+                  contents="비밀번호 확인"
+                  placeholder="비밀번호 확인"
+                  purpose="sign-up"
+                  size="small"
+                  require
+                />
 
-                        <div className='sign-up-name-wrap'>
-                          <Input type='text' contents='닉네임' placeholder='닉네임' purpose="sign-up" size="small" require/>
-                          <span>영문, 한글, 숫자 2~10자리</span>
-                        </div>
+                <div className="sign-up-name-wrap">
+                  <Input
+                    type="text"
+                    contents="닉네임"
+                    placeholder="닉네임"
+                    purpose="sign-up"
+                    size="small"
+                    require
+                  />
+                  <Button
+                    type="button"
+                    content="중복확인"
+                    color="primary"
+                    size="small"
+                  />
+                  <span>영문, 한글, 숫자 2~10자리</span>
+                </div>
 
-                        <div className='sign-up-birth-wrap'>
-                        <Input type='text' contents='생년월일' placeholder='생년월일' purpose="sign-up" size="small" require/>
-                        <span>생년월일 숫자 8자리 (ex: 19840326)</span>
-                        </div>
-                        
-                        <input type='radio' ></input>
+                <div className="sign-up-birth-wrap">
+                  <Input
+                    type="text"
+                    contents="생년월일"
+                    placeholder="생년월일"
+                    purpose="sign-up"
+                    size="small"
+                    require
+                  />
+                  <span>생년월일 숫자 8자리 (ex: 19840326)</span>
+                </div>
 
-                        <div className='sign-up-event-wrap'>
-                        <Input type='text' contents='프로모션코드'
-                          placeholder='프로모션코드(선택)' purpose="sign-up" size="small"/>
-                        <span>제휴 이벤트 쿠폰번호는 가입 후 마이페이지에서 등록 가능</span>
-                        </div>
-                        </div>
-                </fieldset>
-            </form>
+                <div className="sign-up-radio-wrap">
+                  <span className={`radio-text require`}>성별</span>
+                  <RadioGroup data={RADIO_DATA} onChange={handleRadioChange} />
+                </div>
+
+                <div className="sign-up-event-wrap">
+                  <Input
+                    type="text"
+                    contents="프로모션코드"
+                    placeholder="프로모션코드(선택)"
+                    purpose="sign-up"
+                    size="small"
+                  />
+                  <span>
+                    제휴 이벤트 쿠폰번호는 가입 후 마이페이지에서 등록 가능
+                  </span>
+                </div>
+              </div>
+            </fieldset>
+          </form>
         </section>
-        <section>
-
-        </section>
+        <section></section>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
