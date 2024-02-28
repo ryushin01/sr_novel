0ed10ac9c65d13d10066a62fa8f306b56569f69c
박요진 <syon013@naver.com>
2024-02-28 11:37:48 +0900

    💄 평가관련 컴포넌트 생성

---
+++ src/components/BookEval/BookEval.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './BookEval.scss';
+
+const BookEval = ({ isbn13 }) => {
+  const [bookDetailData, setBookDetailData] = useState([]);
+
+  useEffect(() => {
+    getDetailData();
+  }, []);
+
+  const getDetailData = async () => {
+    try {
+      const response = await axios.get('../data/DetailData.json');
+
+      if (isbn13 % 3 === 0) {
+        setBookDetailData(
+          response?.data?.result.filter(data => data.type === 'A'),
+        );
+      } else if (isbn13 % 3 === 1) {
+        setBookDetailData(
+          response?.data?.result.filter(data => data.type === 'B'),
+        );
+      } else {
+        setBookDetailData(
+          response?.data?.result.filter(data => data.type === 'C'),
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return bookDetailData.map(el => {
+    return (
+      <div key={el.views} className="list-item-eval-wrap">
+        <ul>
+          <li>{el.views.toLocaleString()}</li>
+          <li>{el.interests.toLocaleString()}</li>
+          <li>{el.totalRating}</li>
+        </ul>
+      </div>
+    );
+  });
+};
+
+export default BookEval;
