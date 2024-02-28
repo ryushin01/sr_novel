55037e30e14efc05660d0c9744bc50770eebd2a8
박요진 <syon013@naver.com>
2024-02-28 18:09:23 +0900

    🎨 조회수 및 관심작품 단위 (천, 만 등..) 줄여서 나오도록 Intl.NumberFormat 함수 추가

--- src/pages/BookDetail/components/BookDetailInfo/DetailInfoEvalWrap.jsx
+++ src/pages/BookDetail/components/BookDetailInfo/DetailInfoEvalWrap.jsx
@@ -1,8 +1,16 @@
 const DetailInfoEvalWrap = ({ data }) => {
+  // Intl 객체를 이용하여 만 이상의 숫자를 2만과 같이 표현
+  const compactNumberFormatter = new Intl.NumberFormat('ko', {
+    notation: 'compact',
+  });
+
+  const compactNumber = num => {
+    return compactNumberFormatter.format(num);
+  };
   return (
     <div className="book-detail-info-eval-wrap">
-      <span>조회수 {data.views.toLocaleString()}</span>
-      <span>관심작품 {data.interests.toLocaleString()}</span>
+      <span>조회수 {compactNumber(data.views)}</span>
+      <span>관심작품 {compactNumber(data.interests)}</span>
       <span>별점 {data.totalRating}</span>
       <span>댓글 {data.totalComments.toLocaleString()}</span>
     </div>
