0cd72aa01ac3b57ad85ba755dcc000ad20eb4591
박요진 <syon013@naver.com>
2024-02-29 11:37:03 +0900

    ✨Detail Page 상세 필터 기능 추가

--- src/pages/BookDetail/components/FilterButton/FilterButton.jsx
+++ src/pages/BookDetail/components/FilterButton/FilterButton.jsx
@@ -1,10 +1,20 @@
-const FilterButton = () => {
+import './FilterButton.scss';
+
+const FilterButton = ({ episodeSort, setEpisodeSort }) => {
   return (
     <div className="book-detail-filter-btn-wrap">
-      <button type="button" className="book-detail-filter-btn-1">
+      <button
+        type="button"
+        className={`book-detail-filter-btn-1 ${episodeSort === 'last' && 'active'}`}
+        onClick={() => setEpisodeSort('last')}
+      >
         최신화부터
       </button>
-      <button type="button" className="book-detail-filter-btn-2">
+      <button
+        type="button"
+        className={`book-detail-filter-btn-2 ${episodeSort === 'first' && 'active'}`}
+        onClick={() => setEpisodeSort('first')}
+      >
         첫화부터
       </button>
     </div>
